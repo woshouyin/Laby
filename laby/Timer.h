f504b779309215096c331bad3c967883fe3b2d22
woshouyin <1023751276@qq.com>
2018-01-08 14:36:07 +0800

    hello

--- laby/Timer.h
+++ laby/Timer.h
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #pragma once
 
 //Hello it is a timer 2.0
@@ -9,16 +8,3 @@ public:
 	long lastTick;
 	Timer(int pause);
 	bool Loop();
-=======
-#pragma once
-
-//Hello it is a timer
-
-class Timer {
-public:
-	int pause;
-	long lastTick;
-	Timer(int pause);
-	bool Loop();
->>>>>>> 92cf196007161751d184c07a40bd150dbec458d8
-};
